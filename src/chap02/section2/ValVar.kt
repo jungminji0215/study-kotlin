522f489493458a9f309d305632ec8af180f0283c
jungminji <wjdalswl0215@gmail.com>
2022-03-30 00:08:48 +0900

    val, var 차이 학습

---
+++ src/chap02/section2/ValVar.kt
@@ -0,0 +1,11 @@
+package chap02.section2
+
+fun main(){
+    val number = 10 // Int형으로 추론
+    var language = "korean"
+    val secondNumber : Int = 20
+
+    language = "English"
+
+    println("$number")
+}
