c901cb2ed03e00d2cd5b5ad2674d2f282346b517
jungminji <wjdalswl0215@gmail.com>
2022-04-03 18:04:11 +0900

    val var 차이

---
+++ src/chap02/VarVal.kt
@@ -0,0 +1,10 @@
+package chap02
+
+fun VarVal() {
+    var a: Int = 5
+    println(a)
+}
+
+fun main(){
+    VarVal()
+}
--- src/chap02/section2/StringTest.kt
+++ src/chap02/section2/StringTest.kt
@@ -1,6 +1,8 @@
 package chap02.section2
 
 fun main(){
+    // val : 선언시에만 초기화 가능!
+    // 중간에 값을 변경할 수 없음
     var str1 : String = "Hello"
     var str2 = "World"
     var str3 = "Hello"
