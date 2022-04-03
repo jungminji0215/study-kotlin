a7426948aa3d3f32b04d7fddfec3ce3573b3d23d
jungminji <wjdalswl0215@gmail.com>
2022-04-03 18:12:17 +0900

    Array

---
+++ src/ArrayEx01.kt
@@ -0,0 +1,9 @@
+fun ArrayEx01() {
+    // Array<T>
+    var intArr = arrayOf(1,2,3)
+    println(intArr[0])
+}
+
+fun main(){
+    ArrayEx01()
+}
--- src/TypeChange.kt
+++ src/TypeChange.kt
@@ -1,6 +1,6 @@
 fun TypeChange() {
     var a: Int = 1
-    // 명시적 형변환 - 개발자가 직접 
+    // 명시적 형변환 - 개발자가 직접
     // 암시적 형변환은 코틀린이 지원하지 않음
     var b: Long = a.toLong()
 }
